c1acf45fd9b6bbc8d548b69a74a00a9fc016ea48
jiarenjie <164977873@qq.com>
2017-07-06 20:02:21 +0800

    ver 2.0

--- src/delimited_reconcile_file.erl
+++ src/delimited_reconcile_file.erl
@@ -13,42 +13,60 @@
 -export([parse/2]).
 
 parse(Config,Bin) ->
-  BinListDetail = content_pase(Config,Bin),
-  %lager:debug("line:~p",[length(BinListDetail)]),
-  [line_to_map(Line,Config) || Line <- BinListDetail].
-
-content_pase(Config,Bin) ->
   Delimit_line = maps:get(delimit_line, Config, undefined),
   BinList = binary:split(Bin, Delimit_line, [global, trim]),
   TotalLines = length(BinList),
-  TopSkipLines = maps:get(topLines, Config, undefined),
-  BottomSkipLines = maps:get(detailLines, Config, undefined),
-  BottomTopLines = maps:get(topLines, Config, undefined),
+  SkipTopLines = maps:get(skipTopLines, Config, undefined),
+  SkipEndLines = maps:get(skipEndLines, Config, undefined),
+  Delimit_field = maps:get(delimit_field, Config, undefined),
+  Field_map = maps:get(field_map, Config, undefined),
+  HeadLine = maps:get(headLine, Config, undefined),
+  BinHeadLine = get_file_head(BinList,HeadLine),
 
-  case maps:get(separation_line, Config, undefined) of
-    undefined ->
-      lists:sublist(BinList, BottomTopLines+1, TotalLines-TopSkipLines-BottomSkipLines);
-    Separation_line when is_integer(Separation_line) ->
-      Binary_separation_line = lists:nth(Separation_line,BinList),
-      BinListDetail = lists:sublist(BinList, BottomTopLines+1, TotalLines-TopSkipLines-BottomSkipLines),
-      F = fun_filter_line(Binary_separation_line),
-      lists:filter(F , BinListDetail)
-  end.
+  Separation_line = maps:get(separation_line, Config, undefined),
+
+  BinContent = get_content(BinList,SkipTopLines,TotalLines,SkipEndLines,Separation_line),
+
+  ListHeadLine = headLine_to_list(BinHeadLine,hd(BinContent),Delimit_field),
+
+  %lager:debug("line:~p",[length(BinListDetail)]),
+  [line_to_map(Line,Delimit_field,ListHeadLine,Field_map) || Line <- BinContent].
+
+
+get_file_head(BinList,HeadLine) when is_integer(HeadLine)->
+  lists:nth(HeadLine,BinList);
+get_file_head(BinList,undefined) ->
+  <<>>.
+
+get_content(BinList,SkipTopLines,TotalLines,SkipEndLines,Separation_line) when is_integer(Separation_line)->
+  Binary_separation_line = lists:nth(Separation_line,BinList),
+  BinListDetail = lists:sublist(BinList, SkipTopLines+1, TotalLines-SkipTopLines-SkipEndLines),
+  F = fun_filter_line(Binary_separation_line),
+  lists:filter(F , BinListDetail)
+;
+get_content(BinList,SkipTopLines,TotalLines,SkipEndLines,undefined) ->
+  lists:sublist(BinList, SkipTopLines+1, TotalLines-SkipTopLines-SkipEndLines).
+
+headLine_to_list(<<>>,L,Delimit_field)->
+  Length = length(binary:split(L, Delimit_field, [global])),
+  Lists = lists:seq(1,Length),
+  lists:map(fun(L)-> I = integer_to_binary(L),<<"column",I/binary>> end,Lists);
+headLine_to_list(BinHeadLine,_L,Delimit_field) when is_binary(BinHeadLine)->
+
+  binary:split(BinHeadLine, Delimit_field, [global]).
 
 fun_filter_line(L) ->
   fun(T) ->
     T =/= L
   end.
-line_to_map(Line, Config) when is_binary(Line) ->
-  Delimit_field = maps:get(delimit_field, Config, undefined),
+line_to_map(Line, Delimit_field,BinHeadLine,Field_map) when is_binary(Line) ->
   L = binary:split(Line, Delimit_field, [global]),
-  File_head = maps:get(file_head, Config, undefined),
-  List = lists:zip(File_head,L),
-  Field_map = maps:get(field_map, Config, undefined),
+  List = lists:zip(BinHeadLine,L),
   Keys = maps:keys(Field_map),
   maps:from_list([ to_list(X,List,maps:get(X,Field_map)) || X<-Keys]).
 
 to_list(X, List, Value) when is_tuple(Value) ->
+
   {Total,Text_identifier} = Value,
   Val = proplists:get_value(Total,List),
   Field = binary:replace(Val, Text_identifier,<<"">>, [global]),
@@ -56,3 +74,4 @@ to_list(X, List, Value) when is_tuple(Value) ->
 to_list(X, List, FieldMap) when is_binary(FieldMap) ->
   Val = proplists:get_value(FieldMap,List),
   {X,Val}.
+
--- src/test/delimited_reconcile_file_test.erl
+++ src/test/delimited_reconcile_file_test.erl
@@ -14,178 +14,70 @@
 -compile(export_all).
 
 config()->
-  #{file_head => [<<"清算日期"/utf8>>
-    ,<<"交易时间"/utf8>>
-    ,<<"受理商户ID"/utf8>>
-    ,<<"受理商户多应用"/utf8>>
-    ,<<"结算商户编号"/utf8>>
-    ,<<"结算商户多应用类型"/utf8>>
-    ,<<"交易终端号"/utf8>>
-    ,<<"交易金额"/utf8>>
-    ,<<"商户手续费"/utf8>>
-    ,<<"收单收益"/utf8>>
-    ,<<"品牌费"/utf8>>
-    ,<<"交易类型"/utf8>>
-    ,<<"卡号"/utf8>>
-    ,<<"发卡机构"/utf8>>
-    ,<<"卡种"/utf8>>
-    ,<<"收单机构BIT32"/utf8>>
-    ,<<"终端流水号BIT11"/utf8>>
-    ,<<"银联系统跟踪号"/utf8>>
-    ,<<"REF_NO"/utf8>>
-    ,<<"授权号"/utf8>>
-    ,<<"原终端流水号"/utf8>>
-    ,<<"原银联系统跟踪号"/utf8>>
-    ,<<"源BIT37"/utf8>>
-    ,<<"UMS_MCC"/utf8>>
-    ,<<"是否两条线垫支"/utf8>>
-    ,<<"是否分润"/utf8>>
-    ,<<"受理点商户号"/utf8>>
-    ,<<"商户多应用类型"/utf8>>
-    ,<<"受理点分润金额"/utf8>>
-    ,<<"机构分润金额"/utf8>>
-    ,<<"是否清算"/utf8>>
-    ,<<"受理点终端编号"/utf8>>
-    ,<<"交易发起渠道"/utf8>>
-    ,<<"发送机构标识码"/utf8>>
-    ,<<"原清算日期"/utf8>>
-    ,<<"服务点输入方式"/utf8>>
-    ,<<"T0标记"/utf8>>
-    ,<<"T0手续费"/utf8>>
-    ,<<"新增字段3"/utf8>>
-    ,<<"借贷记标记"/utf8>>
-    ,<<"交换费"/utf8>>
-    ,<<"转接清算费"/utf8>>
-    ,<<"新增字段7"/utf8>>
-    ,<<"新增字段8"/utf8>>
-    ,<<"新增字段9"/utf8>>
-    ,<<"订单号"/utf8>>
-
-  ]
-    ,field_map => #{<<"settleDate">> => {<<"清算日期"/utf8>>,[<<"\"">>]}
-    ,<<"txnDate">> => {<<"交易时间"/utf8>>,[<<"\"">>]}
-    ,<<"termid">> => {<<"结算商户编号"/utf8>>,[<<"\"">>]}
-    ,<<"refid">> => {<<"交易终端号"/utf8>>,[<<"\"">>]}
-    ,<<"txnAmt">> => <<"交易金额"/utf8>>
-  }
+  #{
+    field_map => #{<<"settleDate">> => {<<"清算日期"/utf8>>,[<<"\"">>]}
+      ,<<"txnDate">> => {<<"交易时间"/utf8>>,[<<"\"">>]}
+      ,<<"termid">> => {<<"结算商户编号"/utf8>>,[<<"\"">>]}
+      ,<<"refid">> => {<<"交易终端号"/utf8>>,[<<"\"">>]}
+      ,<<"txnAmt">> => <<"交易金额"/utf8>>
+    }
     ,delimit_field => [<<",">>]
-    ,delimit_line => [<<$\r, $\n>>]
-    ,topLines => 1
-    ,detailLines => 0
+    ,delimit_line => [<<$\n>>]
+    ,skipTopLines => 1
+    ,skipEndLines => 0
+    ,headLine =>1
 
   }.
 
 config3()->
-  #{file_head => [
-    <<"清算日期"/utf8>>
-    ,<<"交易日期"/utf8>>
-    ,<<"交易时间"/utf8>>
-    ,<<"终端号"/utf8>>
-    ,<<"交易金额"/utf8>>
-    ,<<"清算金额"/utf8>>
-    ,<<"手续费"/utf8>>
-    ,<<"流水号"/utf8>>
-    ,<<"交易类型"/utf8>>
-    ,<<"参考号"/utf8>>
-    ,<<"卡号"/utf8>>
-    ,<<"商户号"/utf8>>
-    ,<<"发卡行"/utf8>>
-    ,<<"卡类型"/utf8>>
-    ,<<"商户订单号"/utf8>>
-    ,<<"银商订单号"/utf8>>
-    ,<<"支付类型"/utf8>>
-
-  ]
-    ,field_map => #{
-    <<"settleDate">> => <<"清算日期"/utf8>>
-    ,<<"txnDate">> => <<"交易日期"/utf8>>
-    ,<<"txnTime">> => <<"交易时间"/utf8>>
-    ,<<"txnAmt">> => <<"交易金额"/utf8>>
-    ,<<"req">> => <<"流水号"/utf8>>
-  }
+  #{
+    field_map => #{
+      <<"settleDate">> => <<"清算日期"/utf8>>
+      ,<<"txnDate">> => <<"交易日期"/utf8>>
+      ,<<"txnTime">> => <<"交易时间"/utf8>>
+      ,<<"txnAmt">> => <<"交易金额"/utf8>>
+      ,<<"req">> => <<"流水号"/utf8>>
+    }
     ,delimit_field => [<<"\t">>]
     ,delimit_line => [<<$\r, $\n>>]
-    ,topLines => 4
-    ,detailLines => 2
+    ,skipTopLines => 4
+    ,skipEndLines => 2
+    ,headLine =>4
 
   }.
 
 config4()->
-  #{file_head => [
-    <<"清算日期"/utf8>>
-    ,<<"交易日期"/utf8>>
-    ,<<"交易时间"/utf8>>
-    ,<<"交易金额"/utf8>>
-    ,<<"终端号"/utf8>>
-    ,<<"清算金额"/utf8>>
-    ,<<"手续费"/utf8>>
-    ,<<"流水号"/utf8>>
-    ,<<"交易类型"/utf8>>
-    ,<<"参考号"/utf8>>
-    ,<<"卡号"/utf8>>
-    ,<<"商户号"/utf8>>
-    ,<<"发卡行"/utf8>>
-    ,<<"卡类型"/utf8>>
-    ,<<"商户订单号"/utf8>>
-    ,<<"银商订单号"/utf8>>
-    ,<<"支付类型"/utf8>>
-    ,<<"实际支付金额"/utf8>>
-    ,<<"备注"/utf8>>
-    ,<<"付款附言"/utf8>>
-    ,<<"钱包优惠金额"/utf8>>
-
-  ]
-    ,field_map => #{
-    <<"settleDate">> => <<"清算日期"/utf8>>
-    ,<<"txnDate">> => <<"交易日期"/utf8>>
-    ,<<"txnTime">> => <<"交易时间"/utf8>>
-    ,<<"txnAmt">> => <<"交易金额"/utf8>>
-    ,<<"req">> => <<"流水号"/utf8>>
-  }
+  #{
+    field_map => #{
+      <<"settleDate">> => <<"conclum1">>
+      ,<<"txnDate">> => <<"conclum2">>
+      ,<<"txnTime">> => <<"conclum3"/utf8>>
+      ,<<"txnAmt">> => <<"conclum5"/utf8>>
+      ,<<"req">> => <<"conclum9"/utf8>>
+    }
     ,delimit_field => [<<"\t">>]
     ,delimit_line => [<<$\r, $\n>>]
-    ,topLines => 4
-    ,detailLines => 2
+    ,skipTopLines => 4
+    ,skipEndLines => 2
   }.
 
 config5() ->
-  #{file_head => [
-    <<"head">>
-    ,<<"商户号"/utf8>>
-    ,<<"交易日期"/utf8>>
-    ,<<"交易时间"/utf8>>
-    ,<<"终端号"/utf8>>
-    ,<<"交易类型"/utf8>>
-    ,<<"卡号"/utf8>>
-    ,<<"交易金额"/utf8>>
-    ,<<"清算金额"/utf8>>
-    ,<<"手续费"/utf8>>
-    ,<<"参考号"/utf8>>
-    ,<<"流水号"/utf8>>
-    ,<<"商户名称"/utf8>>
-    ,<<"卡类型"/utf8>>
-    ,<<"商户订单号"/utf8>>
-    ,<<"支付类型"/utf8>>
-    ,<<"银商订单号"/utf8>>
-    ,<<"发卡行"/utf8>>
-    ,<<"rest">>
-
-  ]
-    ,field_map => #{
-    <<"shanghuhao">> => {<<"商户号"/utf8>>,[<<" ">>]}
-    ,<<"txnDate">> => {<<"交易日期"/utf8>>,[<<" ">>]}
-    ,<<"txnTime">> => {<<"交易时间"/utf8>>,[<<" ">>]}
-    ,<<"txnAmt">> => {<<"交易金额"/utf8>>,[<<" ">>,<<"\,">>]}
-    ,<<"req">> => {<<"流水号"/utf8>>,[<<" ">>]}
+  #{field_map => #{
+    <<"shanghuhao">> => {<<"       商户号       "/utf8>>,[<<" ">>]}
+    ,<<"txnDate">> => {<<"   交易日期   "/utf8>>,[<<" ">>]}
+    ,<<"txnTime">> => {<<"   交易时间   "/utf8>>,[<<" ">>]}
+    ,<<"txnAmt">> => {<<"    交易金额    "/utf8>>,[<<" ">>,<<"\,">>]}
+    ,<<"req">> => {<<"    流水号    "/utf8>>,[<<" ">>]}
   }
-    ,delimit_field => [<<226,148,130>>]
     ,delimit_line => [<<"\n">>]
-    ,topLines => 6
-    ,detailLines => 4
+    ,delimit_field => [<<226,148,130>>]
+    ,skipTopLines => 6
+    ,skipEndLines => 4
     ,separation_line => 6
+    ,headLine =>5
   }.
 
-pase_test()->
+pase1_test()->
   Config3 = config3(),
   {ok,Bin3} = file:read_file("tests/20170625.txt.netbank"),
   L3 = delimited_reconcile_file:parse(Config3,Bin3),
@@ -202,9 +94,7 @@ pase_test()->
   {ok,Bin} = file:read_file("tests/test.csv"),
   L = delimited_reconcile_file:parse(Config,Bin),
 
-
-
-  ?assertEqual(length(L),1369),
+  ?assertEqual(length(L),4084),
   ?assertEqual(length(L5),5828),
   ?assertEqual(length(L4),4396),
   ?assertEqual(length(L3),33).
